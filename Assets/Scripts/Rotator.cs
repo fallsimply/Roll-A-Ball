5082bad276978f052056d8c324cf4f9539f1a69b
FALL Simply <simply@fallproject.org>
2019-03-25 15:04:28 -0500

    Initial commit

---
+++ Assets/Scripts/Rotator.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rotator : MonoBehaviour {
+	void Update() {
+		transform.Rotate(new Vector3(15, 30, 45) * Time.deltaTime);
+	}
+	void OnTriggerEnter(Collider other) {
+		if (other.gameObject.CompareTag("Collectable")) {
+
+		}
+	}
+}
