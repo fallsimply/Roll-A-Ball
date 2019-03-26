dc6b2bb2b1e2be9c3e8631851b8b30109569cba9
FALL Simply <simply@fallproject.org>
2019-03-26 14:11:48 -0500

    Finished Base Tutorial (1-8)

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,13 +1,23 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class PlayerController : MonoBehaviour {
 	public float speed;
+	public int marginOfError;
+	public Text countText;
+	public Text winText;
+	private int count;
 	private Rigidbody rb;
+	private int totalCollectables;
 
 	void Start() {
 		rb = GetComponent<Rigidbody>();
+		count = 1;
+		SetTotalCollectables();
+		SetCountText();
+		winText.text = "";
 	}
 
 	void FixedUpdate() {
@@ -16,5 +26,24 @@ public class PlayerController : MonoBehaviour {
 		Vector3 movement = new Vector3(moveHorizontal, 0.0f, moveVertical);
 
 		rb.AddForce(movement * speed);
+	}
+
+	void OnTriggerEnter(Collider other) {
+		if (other.gameObject.CompareTag("Collectable")) {
+			other.gameObject.SetActive(false);
+			count++;
+			SetCountText();
+		}
+	}
+
+	void SetCountText() {
+		countText.text = $"Count: {count.ToString()}";
+		if (count >= (totalCollectables - marginOfError)) {
+			winText.text = "You Win!";
+		}
+	}
+
+	void SetTotalCollectables() {
+		totalCollectables = GameObject.FindGameObjectsWithTag("Collectable").Length;
 	}
 }
