323b947ef6c579e784094e32328967d750f3ef0b
FALL Simply <simply@fallproject.org>
2019-03-28 14:56:49 -0500

    Finished

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -5,7 +5,6 @@ using UnityEngine.UI;
 
 public class PlayerController : MonoBehaviour {
 	public float speed;
-	public int marginOfError;
 	public Text countText;
 	public Text winText;
 	public int[] platforms;
@@ -14,6 +13,7 @@ public class PlayerController : MonoBehaviour {
 	private Rigidbody rb;
 	private int totalCollectables;
 	private int level = 0;
+	private int bad = 0;
 
 
 	void Start() {
@@ -32,24 +32,29 @@ public class PlayerController : MonoBehaviour {
 	}
 
 	void OnTriggerEnter(Collider other) {
-		if (other.gameObject.CompareTag("Collectable")) {
+		if (other.gameObject.CompareTag("Good")) {
 			other.gameObject.SetActive(false);
 			count++;
-			SetCountText();
 			CheckPlatformCompleted();
+		} else if (other.gameObject.CompareTag("Bad")) {
+			other.gameObject.SetActive(false);
+			bad++;
+		} else if (other.gameObject.CompareTag("Best")) {
+			other.gameObject.SetActive(false);
+			count += 3;
 		}
+		SetCountText();
 	}
 
 	void SetCountText() {
-		countText.text = $"Count: {count.ToString()}";
-		if (count >= (totalCollectables - marginOfError)) {
+		countText.text = $"Count: {(count - bad).ToString() }";
+		if (count >= totalCollectables) {
 			winText.text = "You Win!";
 		}
 	}
 
 	void SetTotalCollectables() {
 		totalCollectables = platforms[(platforms.Length - 1)];
-		Debug.Log($"Total Collectables: {totalCollectables}");
 	}
 
 	void ToggleDoor(int index = 0) {
---
+++ Assets/Scripts/Quit.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Quit : MonoBehaviour {
+	public Button button;
+	// Start is called before the first frame update
+	void Start() {
+		button.onClick.AddListener(OnClick);
+	}
+	void OnClick() {
+		Application.Quit();
+	}
+}
